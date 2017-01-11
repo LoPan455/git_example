cdc1b7cd0177caaf6c5041f589a2a68b23acf4a0
Thomas Johander <lopan@ThomasJndersMBP.example.com>
2017-01-11 16:18:14 -0600

    Local Commit to branch_1

---
+++ script.js
@@ -0,0 +1,15 @@
+var thing0 = prompt("What is the first number?");
+confirm("Thing0 is set to " + thing0);
+var thing1 = prompt("What is the second number");
+confirm("Thing1 is set to " + thing1);
+alert("heading into the fucntion, addSomeNumbers...");
+
+
+function = addSomeNumbers( thing0, thing1 ) {
+  alert("Just entered the code block for the function called 'addSomeNumbers'...");
+  thing0 + thing1;
+  alert("The numbers were just added togther,");
+  alert("The value is " + addSomeNumbers);
+
+
+};
