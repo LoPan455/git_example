645412a5bf51270d5b05c47281f63970d2154a3c
thomas <tjohander@gmail.com>
2017-01-13 07:50:57 -0600

    JS works as expected

--- statGen.js
+++ statGen.js
@@ -1,12 +1,14 @@
 
 
 console.log("The script has loaded");
-//confirm("Ready to roll?");
+confirm("Ready to roll?");
+
 //Design the fucntion of the random stat generator
+
 function statGen() {
 //I want to give a range between 0 and 20.  I have to take the decimal of Math.random
-// and multiply it by 20 to get a value that will make sense, but we need a non-decimal //number,
-//so we have to round down.  We're not giving away free lunches here.
+// and multiply it by 20 to get a value that will make sense, but we need a non-decimal //number,so we have to round down to the nearest whole number.
+
   var stat = Math.floor( (Math.random() * 20) );
 //If, however, we want a reasonable value, I want to 're-roll' if we get anything below 6
   if (stat <= 6) {
@@ -20,17 +22,16 @@ function statGen() {
 }
 //Declare the variables for the different stats
 
-/*
 var strStat = statGen();
 var dexStat = statGen();
 var intStat = statGen();
 var wisStat = statGen();
 var conStat = statGen();
 var chaStat = statGen();
-*/
+
 
 //Render the stat values in the browser
-var strStat = statGen();
+
 var elStr = document.getElementById('strength');
 elStr.textContent = strStat;
 
