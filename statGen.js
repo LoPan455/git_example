fbf86009991e4fa584107a6fac2a9789c3d72e26
thomas <tjohander@gmail.com>
2017-02-09 07:36:38 -0600

    working on getting the stat roll into a button

--- statGen.js
+++ statGen.js
@@ -1,7 +1,50 @@
+function statGen() {
+//I want to give a range between 0 and 20.  I have to take the decimal of Math.random
+// and multiply it by 20 to get a value that will make sense, but we need a non-decimal //number,so we have to round down to the nearest whole number.
+
+  var stat = Math.floor( (Math.random() * 20) );
+//If, however, we want a reasonable value, I want to 're-roll' if we get anything below 6
+  if (stat <= 6) {
+    var stat = statGen();
+    return stat;
+//Otherwise, I want to know in the console that it's working, and then I want the value returned
+  } else {
+    console.log("Your stat roll is " + stat);
+    return stat;
+  }
+
+}
 
 
-console.log("The script has loaded");
-confirm("Ready to roll?");
+$(document).ready(function(){
+  $('statRollerBtn').click(function(){
+    console.log("The script has loaded");
+    confirm("Ready to roll?");
+    var strStat = statGen();
+    var dexStat = statGen();
+    var intStat = statGen();
+    var wisStat = statGen();
+    var conStat = statGen();
+    var chaStat = statGen();
+    var elStr = document.getElementById('strength');
+    elStr.textContent = strStat;
+
+    var elStr = document.getElementById('dexterity');
+    elStr.textContent = dexStat;
+
+    var elStr = document.getElementById('intelligence');
+    elStr.textContent = intStat;
+
+    var elStr = document.getElementById('wisdom');
+    elStr.textContent = wisStat;
+
+    var elStr = document.getElementById('constitution');
+    elStr.textContent = conStat;
+
+    var elStr = document.getElementById('charisma');
+    elStr.textContent = chaStat;
+  });
+});
 
 //Design the fucntion of the random stat generator
 
@@ -23,16 +66,11 @@ function statGen() {
 }
 //Declare the variables for the different stats
 
-var strStat = statGen();
-var dexStat = statGen();
-var intStat = statGen();
-var wisStat = statGen();
-var conStat = statGen();
-var chaStat = statGen();
+
 
 
 //Render the stat values in the browser
-
+/*
 var elStr = document.getElementById('strength');
 elStr.textContent = strStat;
 
@@ -50,3 +88,4 @@ elStr.textContent = conStat;
 
 var elStr = document.getElementById('charisma');
 elStr.textContent = chaStat;
+*/
